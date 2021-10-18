dab098c6d245936889edbca647c45d235012ac7a
yushan <yushan@intervest.lk>
2021-10-18 12:15:11 +0530

    feat: Add JWTs to login page in front-end

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -1,37 +1,60 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useToken } from "../auth/useToken";
 
 export const LogInPage = () => {
-    const [errorMessage, setErrorMessage] = useState('');
+  const [token, setToken] = useToken();
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [emailValue, setEmailValue] = useState('');
-    const [passwordValue, setPasswordValue] = useState('');
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const onLogInClicked = async () => {
-        alert('Log in not implemented yet');
+  const onLogInClicked = async () => {
+    let status = null;
+    const response = await axios
+      .post("/api/login", {
+        email: emailValue,
+        password: passwordValue,
+      })
+      .catch((err) => (status = err.response.status));
+
+    if (status === null) {
+      const { token } = response.data;
+      setToken(token);
+      history.push("/");
+    } else {
+      console.log("Response Status:", status);
     }
+  };
 
-    return (
-        <div className="content-container">
-            <h1>Log In</h1>
-            {errorMessage && <div className="fail">{errorMessage}</div>}
-            <input
-                value={emailValue}
-                onChange={e => setEmailValue(e.target.value)}
-                placeholder="someone@gmail.com" />
-            <input
-                type="password"
-                value={passwordValue}
-                onChange={e => setPasswordValue(e.target.value)}
-                placeholder="password" />
-            <hr />
-            <button
-                disabled={!emailValue || !passwordValue}
-                onClick={onLogInClicked}>Log In</button>
-            <button onClick={() => history.push('/forgot-password')}>Forgot your password?</button>
-            <button onClick={() => history.push('/signup')}>Don't have an account? Sign Up</button>
-        </div>
-    );
-}
+  return (
+    <div className="content-container">
+      <h1>Log In</h1>
+      {errorMessage && <div className="fail">{errorMessage}</div>}
+      <input
+        value={emailValue}
+        onChange={(e) => setEmailValue(e.target.value)}
+        placeholder="someone@gmail.com"
+      />
+      <input
+        type="password"
+        value={passwordValue}
+        onChange={(e) => setPasswordValue(e.target.value)}
+        placeholder="password"
+      />
+      <hr />
+      <button disabled={!emailValue || !passwordValue} onClick={onLogInClicked}>
+        Log In
+      </button>
+      <button onClick={() => history.push("/forgot-password")}>
+        Forgot your password?
+      </button>
+      <button onClick={() => history.push("/signup")}>
+        Don't have an account? Sign Up
+      </button>
+    </div>
+  );
+};
