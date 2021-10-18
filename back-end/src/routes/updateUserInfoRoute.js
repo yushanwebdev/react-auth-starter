4114390a2f0e24a49cf701720d3387e4f844c2c7
yushan <yushan@intervest.lk>
2021-10-18 16:00:45 +0530

    feat: Verify JWTs in `updateUserInfoRoute` of back-end

--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -22,5 +22,39 @@ export const updateUserInfoRoute = {
 
     // Bearer fgkfjkdjk.djjdss.djdjdjkdsjdkj
     const token = authorization.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      if (err)
+        return res.status(401).json({ message: "Unable to verify token" });
+
+      const { id } = decoded;
+
+      if (id !== userId)
+        return res
+          .status(403)
+          .json({ message: "Not allowed to update that user's data" });
+
+      const db = getDBConnection("react-auth-db");
+      const result = await db
+        .collection("users")
+        .findOneAndUpdate(
+          { _id: ObjectID(id) },
+          { $set: { info: updates } },
+          { returnOriginal: false }
+        );
+
+      const { email, isVerified, info } = result.value;
+
+      jwt.sign(
+        { id, email, isVerified, info },
+        process.env.JWT_SECRET,
+        { expiresIn: "2d" },
+        () => {
+          if (err) res.status(200).json(err);
+
+          res.status(200).json({ token });
+        }
+      );
+    });
   },
 };
